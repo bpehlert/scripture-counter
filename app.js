842db56945a364ca46a4a9342a9e9663f9b7954d
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-12 22:39:09 -0700

    Added days left of reading

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const userInput = document.querySelector('.userInput');
-const book = document.querySelector('.book');
-const pagesPerDay = document.querySelector('.pagesPerDay');
+const userInput = document.querySelector(".userInput");
+const book = document.querySelector(".book");
+const pagesPerDay = document.querySelector(".pagesPerDay");
+const daysOfReading = document.querySelector(".daysOfReading");
 
 const today = new Date();
 const end = new Date(today.getFullYear(), 11, 31);
-const oneDay = 1000*60*60*24;
-const days = Math.ceil((end.getTime()-today.getTime())/(oneDay)) + 1;
+const oneDay = 1000 * 60 * 60 * 24;
+const days = Math.ceil((end.getTime() - today.getTime()) / oneDay) + 1;
 
 const oT = 1184;
 const nT = 1590;
@@ -14,33 +15,33 @@ const dC = 294;
 const pP = 61;
 
 function handleChange() {
-   let totalPages =''
-   switch (book.value){
-      case 'oT': 
-         totalPages = oT + nT + bM + dC + pP;
-         break;
-      case 'nT': 
-         totalPages = nT + bM + dC + pP;
-         break;
-      case 'bM': 
-         totalPages = bM + dC + pP;
-         break;
-      case 'dC':
-         totalPages = dC + pP;
-         break;
-      case 'pP': 
-         totalPages = pP;
-         break;
-      default: '';
-   }
-   
-   const currentPage = userInput.value;
-   const pagesToRead = userInput.value ? ((totalPages - currentPage) / days) : 0;
-   const precision = pagesToRead === 0 ? 1 : 2;
-   pagesPerDay.textContent = pagesToRead.toPrecision(precision);
+  let totalPages = "";
+  switch (book.value) {
+    case "oT":
+      totalPages = oT + nT + bM + dC + pP;
+      break;
+    case "nT":
+      totalPages = nT + bM + dC + pP;
+      break;
+    case "bM":
+      totalPages = bM + dC + pP;
+      break;
+    case "dC":
+      totalPages = dC + pP;
+      break;
+    case "pP":
+      totalPages = pP;
+      break;
+    default:
+      "";
+  }
 
+  const currentPage = userInput.value;
+  const pagesLeft = totalPages - currentPage;
+  const pagesToRead = userInput.value ? (totalPages - currentPage) / days : 0;
+  const precision = pagesToRead === 0 ? 1 : 2;
+  pagesPerDay.textContent = pagesToRead.toPrecision(precision);
+  daysOfReading.textContent = Math.round(pagesLeft / 7);
 }
 
-userInput.addEventListener('keyup', handleChange);
-
-
+userInput.addEventListener("keyup", handleChange);
